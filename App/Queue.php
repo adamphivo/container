316a1e0e98c44dcdb8fd70fd2a8358ecf3bd6f9b
Adam Phi Vo <adamphivo@gmail.com>
2020-10-12 15:43:54 +0200

    Setting up dev env

--- App/Queue.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Queue extends SplQueue{
-    
-}
--- index.php
+++ index.php
@@ -5,3 +5,21 @@ spl_autoload_register(function ($class) {
     include 'App/'.$class.".php";
 });
 
+// Make our items
+$queue = new SplQueue();
+$storage = new SplObjectStorage();
+$producer = new MessageProducer($queue, $storage);
+$reader = new MessageReader($queue, $storage);
+
+// Create mock messages
+$producer->makeMessage("Premier", "ENG", 1);
+$producer->makeMessage("Deuxieme", "ENG", 1);
+$producer->makeMessage("Troisieme, zut", "ENG", 1);
+
+$reader->treatTopMessage();
+$reader->treatTopMessage();
+$reader->treatTopMessage();
+
+// Check if all is empty after usage
+echo var_dump($storage);
+echo var_dump($queue);
